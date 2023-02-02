0f0c4fea5d144801aeebd3ee5f24e0de20479356
Crowdbotics <engineering@crowdbotics.com>
2023-02-02 00:12:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-lake-dev-60656",
+  url: "https://bitter-lake-dev-60656.botics.co",
+  api: "https://bitter-lake-dev-60656.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
